c6ab3b6b4e1cc8847d267057fb9a2d752b062643
JewishLewish <frostyornot@gmail.com>
2023-01-01 00:21:54 -0800

    Placing 1.0.4.tar-gz

--- layla/setup.py
+++ layla/setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
-classifiers=[
-    "Programming Language :: Python :: 3",
-    "LICENSE :: OSI Approved :: zLib License",
-    "Operating System :: OS Independent",
-    "Inteneded Audience :: Education, Data Science"
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
 ]
 
 setup(
