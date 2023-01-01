b013b28989f147237016a6fbef336d7b5ccf1a45
JewishLewish <frostyornot@gmail.com>
2022-12-31 17:33:31 -0800

    Update.

---
+++ layla/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+classifiers=[
+    "Programming Language :: Python :: 3",
+    "LICENSE :: OSI Approved :: zLib License",
+    "Operating System :: OS Independent",
+    "Inteneded Audience :: Education, Data Science"
+]
+
+setup(
+    name = 'Layla Eval',
+    version = '1.0.0',
+    author= "JewishLewish",
+    description="Math Evaluator that uses C Code over Python",
+    packages=find_packages(),
+    url = "https://github.com/JewishLewish/Layla-Python-Module",
+    long_description=open('README.txt').read()
+)
