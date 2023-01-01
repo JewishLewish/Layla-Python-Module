4793dc28b263a04245ea8347683baa22f527cac0
JewishLewish <frostyornot@gmail.com>
2022-12-31 16:39:14 -0800

    Updated Files.

--- testing.py
+++ testing.py
@@ -4,9 +4,9 @@ import time
 start = time.time()
 for i in range(100000):
     eval(str(i) + "+2")
-print(time.time() - start)
+print(time.time() - start) #~.90 seconds
 
 start=time.time()
 for i in range(100000):
     layla.math(str(i) + "+2")
-print(time.time() - start)
+print(time.time() - start) #~.15 seconds
