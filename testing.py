82da7c56a505c050430ae9abe3a69f7f66711e1d
Lenny <65754609+JewishLewish@users.noreply.github.com>
2022-12-31 17:34:01 -0800

    Updated Benchmark code.

--- testing.py
+++ testing.py
@@ -10,3 +10,23 @@ start=time.time()
 for i in range(100000):
     layla.math(str(i) + "+2")
 print(time.time() - start) #~.15 seconds
+
+start = time.time()
+for i in range(100000):
+    eval(str(i) + "+ abs(" + str(i) + ")")
+print(time.time() - start) #1.04 seconds
+
+start=time.time()
+for i in range(100000):
+    layla.math(str(i) + "+ abs(" + str(i) + ")")
+print(time.time() - start) #~.14 seconds
+
+#start = time.time()
+#for i in range(100000):
+#    eval(str(i) + "+ sin(" + str(i) + ")")
+#print(time.time() - start) #Doesn't support sin() evalulations 
+
+start=time.time()
+for i in range(100000):
+    layla.math(str(i) + "+ sin(" + str(i) + ")")
+print(time.time() - start) #~.15 Seconds and Supports it. 
